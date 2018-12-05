ba6905142ff0e8d1b6e76878aa4e66bf6ba9c89c
shixiaogang <shixiaogangg@gmail.com>
2018-12-05 12:45:55 +0800

    [hotfix] Replace the dependencies in the Tencent blacklist

--- tools/travis/stage.sh
+++ tools/travis/stage.sh
@@ -56,7 +56,6 @@ flink-queryable-state/flink-queryable-state-client-java"
 MODULES_CONNECTORS="\
 flink-contrib/flink-connector-wikiedits,\
 flink-filesystems/flink-hadoop-fs,\
-flink-filesystems/flink-mapr-fs,\
 flink-filesystems/flink-s3-fs-base,\
 flink-filesystems/flink-s3-fs-hadoop,\
 flink-filesystems/flink-s3-fs-presto,\
